07b3b5995b4cbcc9a703f51da4ae239accf314cd
li.zhirong <li.zhirong@trs.com.cn>
2018-10-10 15:18:17 +0800

    添加网页第三方分享功能

--- app/src/main/java/com/potato/zhbj/MyApplication.java
+++ app/src/main/java/com/potato/zhbj/MyApplication.java
@@ -3,6 +3,7 @@ package com.potato.zhbj;
 import android.app.Application;
 
 import com.lzy.okgo.OkGo;
+import com.mob.MobSDK;
 
 /**
  * Created by li.zhirong on 2018/9/13/013 17:27
@@ -12,5 +13,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         OkGo.getInstance().init(this);
+        MobSDK.init(this);
     }
 }
--- app/src/main/java/com/potato/zhbj/activity/WebActivity.java
+++ app/src/main/java/com/potato/zhbj/activity/WebActivity.java
@@ -22,6 +22,9 @@ import android.widget.ProgressBar;
 import com.potato.zhbj.R;
 import com.potato.zhbj.utils.PrefUtil;
 
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.OnekeyShareTheme;
+
 /**
  * Created by li.zhirong on 2018/10/9/009 10:30
  */
@@ -122,6 +125,7 @@ public class WebActivity extends AppCompatActivity implements View.OnClickListen
                 finish();
                 break;
             case R.id.btn_share:
+                showShare();
                 break;
             case R.id.btn_textsize:
                 //展示选择字体得弹窗
@@ -129,6 +133,32 @@ public class WebActivity extends AppCompatActivity implements View.OnClickListen
                 break;
         }
     }
+
+    /**
+     * 第三方分享
+     */
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        //样式
+        oks.setTheme(OnekeyShareTheme.CLASSIC);
+        // title标题，微信、QQ和QQ空间等平台使用
+        oks.setTitle("智慧北京");
+        // titleUrl QQ和QQ空间跳转链接
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url在微信、微博，Facebook等平台中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网使用
+        oks.setComment("我是测试评论文本");
+        // 启动分享GUI
+        oks.show(this);
+    }
+
 
     /**
      * 字体设置弹框
